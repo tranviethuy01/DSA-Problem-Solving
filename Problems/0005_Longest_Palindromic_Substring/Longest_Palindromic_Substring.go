3ea324cb581214af4e364128b53017d9fa12a5e6
Huy's Tre64 <tranviethuy01@gmail.com>
2024-02-17 21:58:08 +0700

    update longest palindromic substring

--- Problems/0005_Longest_Palindromic_Substring/Longest_Palindromic_Substring.go
+++ Problems/0005_Longest_Palindromic_Substring/Longest_Palindromic_Substring.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"time"
+	"strings"
 )
 
-// approach: dynamic programming
+// approach: Expand From Center
 // Time complexity: O(n^2).
 // Space complexity: O(1)
-func longestPalindrome_DP(s string) string {
+func longestPalindrome_ExpandFromCenter(s string) string {
 	if len(s) <= 1 {
 		return s
 	}
@@ -43,6 +44,43 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+//===== approach: dynamic programming
+// approach: dynamic programming
+//Time complexity : O(n^2). 
+//Space complexity : O(n^2). It uses O(n^2) space to store the table.
+func longestPalindrome_DP(s string) string {
+    if len(s) <= 1 {
+        return s
+    }
+    
+    maxLen := 1
+    maxStr := s[0:1]
+    dp := make([][]bool, len(s))
+    for i := range dp {
+        dp[i] = make([]bool, len(s))
+    }
+    
+    for i := 0; i < len(s); i++ {
+        dp[i][i] = true
+        for j := 0; j < i; j++ {
+            if s[j] == s[i] && (i-j <= 2 || dp[j+1][i-1]) {
+                dp[j][i] = true
+                if i-j+1 > maxLen {
+                    maxLen = i - j + 1
+                    maxStr = s[j : i+1]
+                }
+            }
+        }
+    }
+    
+    return maxStr
+}
+
+
+
+//=====
+
 
 //===== approach : brute force
 //Time complexity: O(n^3).
@@ -81,6 +119,91 @@ func isPalindrome(s string) bool {
 
 //=====
 
+//===== approach: Manacher approach
+//Time complexity : O(n). Since expanding a palindrome around its center could take O(n) time, the overall complexity is O(n).
+//Space complexity : O(n). It uses O(n) space to store the table.
+
+func longestPalindrome_Manacher(s string) string {
+    if len(s) <= 1 {
+        return s
+    }
+    
+    maxLen := 1
+    maxStr := s[0:1]
+    
+    s = "#" + strings.Join(strings.Split(s, ""), "#") + "#"
+    dp := make([]int, len(s))
+    center := 0
+    right := 0
+    
+    for i := 0; i < len(s); i++ {
+        if i < right {
+            dp[i] = min(right-i, dp[2*center-i])
+        }
+        
+        for i-dp[i]-1 >= 0 && i+dp[i]+1 < len(s) && s[i-dp[i]-1] == s[i+dp[i]+1] {
+            dp[i]++
+        }
+        
+        if i+dp[i] > right {
+            center = i
+            right = i + dp[i]
+        }
+        
+        if dp[i] > maxLen {
+            maxLen = dp[i]
+            maxStr = strings.ReplaceAll(s[i-dp[i]:i+dp[i]+1], "#", "")
+        }
+    }
+    
+    return maxStr
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+
+//=====
+
+
+//===== approach : recursive 
+//Time complexity : O(n^3).
+//Space complexity : O(n).
+
+func longestPalindrome_Recursive(s string) string {
+    if isPalindrome_R(s) {
+        return s
+    }
+    
+    left := longestPalindrome_Recursive(s[1:])
+    right := longestPalindrome_Recursive(s[:len(s)-1])
+    
+    if len(left) > len(right) {
+        return left
+    } else {
+        return right
+    }
+}
+
+func isPalindrome_R(s string) bool {
+    for i := 0; i < len(s)/2; i++ {
+        if s[i] != s[len(s)-1-i] {
+            return false
+        }
+    }
+    return true
+}
+
+
+
+
+//===== 
+
+
 func main() {
 	timeStartWholeProgram := time.Now()
 	testInput := []TestCase{
@@ -108,13 +231,39 @@ func main() {
 		fmt.Println("Correct result is ", value.Result)
 		fmt.Println("TimeLapse", timeLapse)
 
-		fmt.Println("Solution 2: use brute force solution")
+		fmt.Println("Solution 2: use Expand From Center")
+		timeStart = time.Now()
+		result = longestPalindrome_ExpandFromCenter(value.S)
+		timeLapse = time.Since(timeStart)
+		fmt.Println(">Solution result", result)
+		fmt.Println("Correct result is ", value.Result)
+		fmt.Println("TimeLapse", timeLapse)
+
+		fmt.Println("Solution 3: use brute force solution")
 		timeStart = time.Now()
 		result = longestPalindrome_BruteForce(value.S)
 		timeLapse = time.Since(timeStart)
 		fmt.Println(">Solution result", result)
 		fmt.Println("Correct result is ", value.Result)
 		fmt.Println("TimeLapse", timeLapse)
+
+		fmt.Println("Solution 4: use Manacher approach")
+		timeStart = time.Now()
+		result = longestPalindrome_Manacher(value.S)
+		timeLapse = time.Since(timeStart)
+		fmt.Println(">Solution result", result)
+		fmt.Println("Correct result is ", value.Result)
+		fmt.Println("TimeLapse", timeLapse)
+
+		fmt.Println("Solution 5: use Recurisve approach")
+		timeStart = time.Now()
+		result = longestPalindrome_Recursive(value.S)
+		timeLapse = time.Since(timeStart)
+		fmt.Println(">Solution result", result)
+		fmt.Println("Correct result is ", value.Result)
+		fmt.Println("TimeLapse", timeLapse)
+
+
 
 	}
 
@@ -131,38 +280,75 @@ type TestCase struct {
 /*
 
 ===============
-Test count  0 for node {babad
+Test count  0 for node {babad 
                 bab
             }
 Solution 1: use DP
->Solution result aba
-Correct result is
-                bab
-
-TimeLapse 908ns
-Solution 2: use brute force solution
 >Solution result bab
-Correct result is
+Correct result is  
                 bab
-
-TimeLapse 8.963µs
+            
+TimeLapse 13.593µs
+Solution 2: use Expand From Center
+>Solution result aba
+Correct result is  
+                bab
+            
+TimeLapse 963ns
+Solution 3: use brute force solution
+>Solution result bab
+Correct result is  
+                bab
+            
+TimeLapse 963ns
+Solution 4: use Manacher approach
+>Solution result bab
+Correct result is  
+                bab
+            
+TimeLapse 19.389µs
+Solution 5: use Recurisve approach
+>Solution result bab
+Correct result is  
+                bab
+            
+TimeLapse 667ns
 ===============
-Test count  1 for node {cbbd
+Test count  1 for node {cbbd 
                 bb
             }
 Solution 1: use DP
 >Solution result bb
-Correct result is
+Correct result is  
                 bb
-
-TimeLapse 352ns
-Solution 2: use brute force solution
+            
+TimeLapse 1.24µs
+Solution 2: use Expand From Center
 >Solution result bb
-Correct result is
+Correct result is  
                 bb
-
-TimeLapse 297ns
+            
+TimeLapse 463ns
+Solution 3: use brute force solution
+>Solution result bb
+Correct result is  
+                bb
+            
+TimeLapse 426ns
+Solution 4: use Manacher approach
+>Solution result bb
+Correct result is  
+                bb
+            
+TimeLapse 13.371µs
+Solution 5: use Recurisve approach
+>Solution result bb
+Correct result is  
+                bb
+            
+TimeLapse 463ns
 ===============
-TimeLapse Whole Program 425.091µs
+TimeLapse Whole Program 795.577µs
+
 
 */
