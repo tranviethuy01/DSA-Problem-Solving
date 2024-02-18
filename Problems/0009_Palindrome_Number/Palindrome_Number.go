82f5e444d8f132d7f51a74e18ddbf6c01c3ca76b
Huy's Tre64 <tranviethuy01@gmail.com>
2024-02-18 20:35:08 +0700

    update more code

--- Problems/0009_Palindrome_Number/Palindrome_Number.go
+++ Problems/0009_Palindrome_Number/Palindrome_Number.go
@@ -2,36 +2,35 @@ package main
 
 import (
 	"fmt"
-	
+
+	"strconv"
 	"time"
-  "strconv"
 )
 
 // Simplest: approach: reverse digit of number, then compare the reversed number with the origin number
 
 func isPalindrome(x int) bool {
-    // Special cases:
-    // If x is negative or ends with 0 (except for 0 itself), it cannot be a palindrome.
-    if x < 0 || (x%10 == 0 && x != 0) {
-        return false
-    }
+	// Special cases:
+	// If x is negative or ends with 0 (except for 0 itself), it cannot be a palindrome.
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
 
-    // Reverse half of the number.
-    reversed := 0
-    for x > reversed {
-        reversed = reversed*10 + x%10
-        x /= 10
-    }
+	// Reverse half of the number.
+	reversed := 0
+	for x > reversed {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
 
-    // If the number of digits is odd, we can ignore the middle digit.
-    // For example, if x = 12321, at the end of the loop we would have
-    // reversed = 123, x = 12 (middle digit is 2 which can be ignored).
-    // For even digits, reversed and x would be of the same length.
-    return x == reversed || x == reversed/10
+	// If the number of digits is odd, we can ignore the middle digit.
+	// For example, if x = 12321, at the end of the loop we would have
+	// reversed = 123, x = 12 (middle digit is 2 which can be ignored).
+	// For even digits, reversed and x would be of the same length.
+	return x == reversed || x == reversed/10
 }
 
-
-// approach : reverse number 
+// approach : reverse number
 
 func checkPalindrome_ReverseNumber(n int) bool {
 	reverse := 0
@@ -43,50 +42,43 @@ func checkPalindrome_ReverseNumber(n int) bool {
 	return reverse == n
 }
 
-
-
-
-//===== approach: convert to string then iterate through haft of the string 
+//===== approach: convert to string then iterate through haft of the string
 
 func isPalindrome_ConvertToString(x int) bool {
-    // Convert the integer to a string.
-    str := strconv.Itoa(x)
-    
-    // Iterate through half of the string.
-    n := len(str)
-    for i := 0; i < n/2; i++ {
-        // Compare characters from the beginning and end of the string.
-        if str[i] != str[n-1-i] {
-            return false
-        }
-    }
-    return true
+	// Convert the integer to a string.
+	str := strconv.Itoa(x)
+
+	// Iterate through half of the string.
+	n := len(str)
+	for i := 0; i < n/2; i++ {
+		// Compare characters from the beginning and end of the string.
+		if str[i] != str[n-1-i] {
+			return false
+		}
+	}
+	return true
 }
 
 //=====
-
 
 //===== approach : check for a string
 //str = "112233445566778899000000998877665544332211"
 
 func isPalindrome_CheckString(str string) bool {
-    // Iterate through half of the string.
-    n := len(str)
-    for i := 0; i < n/2; i++ {
-        // Compare characters from the beginning and end of the string.
-        if str[i] != str[n-1-i] {
-            return false
-        }
-    }
-    return true
+	// Iterate through half of the string.
+	n := len(str)
+	for i := 0; i < n/2; i++ {
+		// Compare characters from the beginning and end of the string.
+		if str[i] != str[n-1-i] {
+			return false
+		}
+	}
+	return true
 }
 
 //=====
 
-
-
-//===== recursive solution 
-//
+// ===== recursive solution
 //
 // Function to check if a number is palindrome.
 func isPalindrome_Recursive(num int) bool {
@@ -101,7 +93,6 @@ func isPalindrome_Recursive(num int) bool {
 	// Call the recursive function to check if the number is palindrome.
 	return isPalindrom_RecursiveUtil(num, &dupNum)
 }
-
 
 // A recursive function to check whether num is palindrome or not.
 // num contains the current number being checked.
@@ -124,7 +115,7 @@ func isPalindrom_RecursiveUtil(num int, dupNum *int) bool {
 	return num%10 == *dupNum%10
 }
 
-//===== 
+//=====
 
 func main() {
 	timeStartWholeProgram := time.Now()
@@ -142,46 +133,42 @@ func main() {
              `,
 		},
 		{
-			N:  10,
+			N: 10,
 			Result: `
 			false
             `,
 		},
-  {
-			N:  2147483647,
+		{
+			N: 2147483647,
 			Result: `
 			false
             `,
 		},
-    {
+		{
 			N: 9223372036854775806,
 			Result: `
 			false
             `,
 		},
-{
+		{
 			N: 9223372036854775807,
 			Result: `
 			false
             `,
 		},
-{
+		{
 			N: 9223372036854775008,
 			Result: `
       overflow case: false
             `,
 		},
-{
-	S: "112233445566778899000000998877665544332211",
+		{
+			S: "112233445566778899000000998877665544332211",
 			Result: `
       Test case with String => true
 
             `,
 		},
-
-
-
-
 	}
 	for count, value := range testInput {
 		fmt.Println("===============")
@@ -218,8 +205,6 @@ func main() {
 		fmt.Println("Correct result is ", value.Result)
 		fmt.Println("TimeLapse", timeLapse)
 
-
-
 	}
 
 	timeLapsedWholeProgram := time.Since(timeStartWholeProgram)
@@ -229,247 +214,245 @@ func main() {
 
 type TestCase struct {
 	S      string
-  N int
+	N      int
 	Result string
 }
 
 /*
 
 ===============
-Test count  0 for node { 121 
+Test count  0 for node { 121
           true
             }
 Solution 1: use
 >Solution result true
-Correct result is  
+Correct result is
           true
-            
+
 TimeLapse 537ns
 Solution 2: use convert to string
 >Solution result true
-Correct result is  
+Correct result is
           true
-            
+
 TimeLapse 2.13µs
 Solution 3: recursive
 >Solution result true
-Correct result is  
+Correct result is
           true
-            
+
 TimeLapse 704ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
           true
-            
+
 TimeLapse 259ns
 ===============
-Test count  1 for node { -121 
+Test count  1 for node { -121
              false
              }
 Solution 1: use
 >Solution result false
-Correct result is  
+Correct result is
              false
-             
+
 TimeLapse 111ns
 Solution 2: use convert to string
 >Solution result false
-Correct result is  
+Correct result is
              false
-             
+
 TimeLapse 444ns
 Solution 3: recursive
 >Solution result true
-Correct result is  
+Correct result is
              false
-             
+
 TimeLapse 167ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
              false
-             
+
 TimeLapse 111ns
 ===============
-Test count  2 for node { 10 
+Test count  2 for node { 10
 			false
             }
 Solution 1: use
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 111ns
 Solution 2: use convert to string
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 185ns
 Solution 3: recursive
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 148ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 74ns
 ===============
-Test count  3 for node { 2147483647 
+Test count  3 for node { 2147483647
 			false
             }
 Solution 1: use
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 185ns
 Solution 2: use convert to string
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 519ns
 Solution 3: recursive
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 241ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 93ns
 ===============
-Test count  4 for node { 9223372036854775806 
+Test count  4 for node { 9223372036854775806
 			false
             }
 Solution 1: use
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 186ns
 Solution 2: use convert to string
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 7.537µs
 Solution 3: recursive
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 315ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 112ns
 ===============
-Test count  5 for node { 9223372036854775807 
+Test count  5 for node { 9223372036854775807
 			false
             }
 Solution 1: use
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 203ns
 Solution 2: use convert to string
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 592ns
 Solution 3: recursive
 >Solution result false
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 277ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
 			false
-            
+
 TimeLapse 93ns
 ===============
-Test count  6 for node { 9223372036854775008 
+Test count  6 for node { 9223372036854775008
       overflow case: false
             }
 Solution 1: use
 >Solution result false
-Correct result is  
+Correct result is
       overflow case: false
-            
+
 TimeLapse 185ns
 Solution 2: use convert to string
 >Solution result false
-Correct result is  
+Correct result is
       overflow case: false
-            
+
 TimeLapse 537ns
 Solution 3: recursive
 >Solution result false
-Correct result is  
+Correct result is
       overflow case: false
-            
+
 TimeLapse 259ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
       overflow case: false
-            
+
 TimeLapse 74ns
 ===============
-Test count  7 for node {112233445566778899000000998877665544332211 0 
+Test count  7 for node {112233445566778899000000998877665544332211 0
       Test case with String => true
 
             }
 Solution 1: use
 >Solution result true
-Correct result is  
+Correct result is
       Test case with String => true
 
-            
+
 TimeLapse 130ns
 Solution 2: use convert to string
 >Solution result true
-Correct result is  
+Correct result is
       Test case with String => true
 
-            
+
 TimeLapse 315ns
 Solution 3: recursive
 >Solution result true
-Correct result is  
+Correct result is
       Test case with String => true
 
-            
+
 TimeLapse 130ns
 Additional test : test case with string input for large string
 >Solution result true
-Correct result is  
+Correct result is
       Test case with String => true
 
-            
+
 TimeLapse 204ns
 ===============
 TimeLapse Whole Program 1.528271ms
 
 */
 
-
 //REF
 //https://www.geeksforgeeks.org/check-if-a-number-is-palindrome/
-
--- Problems/0010_Regular_Expression_Matching/Regular_Expression_Matching.go
+++ Problems/0010_Regular_Expression_Matching/Regular_Expression_Matching.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 )
+
 //approach: dynamic programing
 //Time Complexity: O(n * m)
 //Space Complexity: O(n * m)
@@ -50,87 +51,81 @@ func isMatch_DP(s string, p string) bool {
 	return dp(0, 0)
 }
 
-
-//===== approach: brute force 
+//===== approach: brute force
 //Time Complexity: Exponential, O(2^(n+m))
 //Space Complexity: O(n + m)
 
 func isMatch_BruteForce(s string, p string) bool {
-    var match func(sIndex, pIndex int) bool
-    match = func(sIndex, pIndex int) bool {
-        if pIndex == len(p) {
-            return sIndex == len(s)
-        }
-        if sIndex == len(s) {
-            if (len(p)-pIndex)%2 == 1 {
-                return false
-            }
-            for i := pIndex + 1; i < len(p); i += 2 {
-                if p[i] != '*' {
-                    return false
-                }
-            }
-            return true
-        }
-        if pIndex+1 < len(p) && p[pIndex+1] == '*' {
-            return (s[sIndex] == p[pIndex] || p[pIndex] == '.') && (match(sIndex+1, pIndex) || match(sIndex, pIndex+2))
-        } else {
-            return (s[sIndex] == p[pIndex] || p[pIndex] == '.') && match(sIndex+1, pIndex+1)
-        }
-    }
-    return match(0, 0)
+	var match func(sIndex, pIndex int) bool
+	match = func(sIndex, pIndex int) bool {
+		if pIndex == len(p) {
+			return sIndex == len(s)
+		}
+		if sIndex == len(s) {
+			if (len(p)-pIndex)%2 == 1 {
+				return false
+			}
+			for i := pIndex + 1; i < len(p); i += 2 {
+				if p[i] != '*' {
+					return false
+				}
+			}
+			return true
+		}
+		if pIndex+1 < len(p) && p[pIndex+1] == '*' {
+			return (s[sIndex] == p[pIndex] || p[pIndex] == '.') && (match(sIndex+1, pIndex) || match(sIndex, pIndex+2))
+		} else {
+			return (s[sIndex] == p[pIndex] || p[pIndex] == '.') && match(sIndex+1, pIndex+1)
+		}
+	}
+	return match(0, 0)
 }
-
-
 
 //=====
 
-//===== approach recursive
-//Time Complexity: Exponential, O(2^(n+m))
-//Space Complexity: O(n + m)
+// ===== approach recursive
+// Time Complexity: Exponential, O(2^(n+m))
+// Space Complexity: O(n + m)
 func isMatch_Recursive(s string, p string) bool {
-    if p == "" {
-        return s == ""
-    }
+	if p == "" {
+		return s == ""
+	}
 
-    firstMatch := s != "" && (p[0] == '.' || s[0] == p[0])
+	firstMatch := s != "" && (p[0] == '.' || s[0] == p[0])
 
-    if len(p) >= 2 && p[1] == '*' {
-        return isMatch_Recursive(s, p[2:]) || (firstMatch && isMatch_Recursive(s[1:], p))
-    }
+	if len(p) >= 2 && p[1] == '*' {
+		return isMatch_Recursive(s, p[2:]) || (firstMatch && isMatch_Recursive(s[1:], p))
+	}
 
-    return firstMatch && isMatch_Recursive(s[1:], p[1:])
+	return firstMatch && isMatch_Recursive(s[1:], p[1:])
 }
 
-
 //=====
-
 
 func main() {
 	timeStartWholeProgram := time.Now()
 	testInput := []TestCase{
 		{
-      S: "aa",
-      P: "a",
+			S: "aa",
+			P: "a",
 			Result: `
           false
             `,
 		},
 		{
-      S: "aa",
-      P: "a*",
+			S: "aa",
+			P: "a*",
 			Result: `
              true
              `,
 		},
 		{
-		  S: "ab",
-      P:".*",
+			S: "ab",
+			P: ".*",
 			Result: `
 			true
             `,
 		},
-		
 	}
 	for count, value := range testInput {
 		fmt.Println("===============")
@@ -159,9 +154,7 @@ func main() {
 		fmt.Println("Correct result is ", value.Result)
 		fmt.Println("TimeLapse", timeLapse)
 
-
-
-  }
+	}
 
 	timeLapsedWholeProgram := time.Since(timeStartWholeProgram)
 	fmt.Println("===============")
@@ -177,70 +170,70 @@ type TestCase struct {
 /*
 
 ===============
-Test count  0 for node {aa a 
+Test count  0 for node {aa a
           false
             }
 Solution 1: use
 >Solution result false
-Correct result is  
+Correct result is
           false
-            
+
 TimeLapse 6.852µs
 Solution 2: use brute force
 >Solution result false
-Correct result is  
+Correct result is
           false
-            
+
 TimeLapse 1.166µs
 Solution 3: use recursive
 >Solution result false
-Correct result is  
+Correct result is
           false
-            
+
 TimeLapse 537ns
 ===============
-Test count  1 for node {aa a* 
+Test count  1 for node {aa a*
              true
              }
 Solution 1: use
 >Solution result true
-Correct result is  
+Correct result is
              true
-             
+
 TimeLapse 2.389µs
 Solution 2: use brute force
 >Solution result true
-Correct result is  
+Correct result is
              true
-             
+
 TimeLapse 389ns
 Solution 3: use recursive
 >Solution result true
-Correct result is  
+Correct result is
              true
-             
+
 TimeLapse 278ns
 ===============
-Test count  2 for node {ab .* 
+Test count  2 for node {ab .*
 			true
             }
 Solution 1: use
 >Solution result true
-Correct result is  
+Correct result is
 			true
-            
+
 TimeLapse 2.259µs
 Solution 2: use brute force
 >Solution result true
-Correct result is  
+Correct result is
 			true
-            
+
 TimeLapse 240ns
 Solution 3: use recursive
 >Solution result true
-Correct result is  
+Correct result is
 			true
-            
+
 TimeLapse 278ns
 ===============
 TimeLapse Whole Program 589.625µs
